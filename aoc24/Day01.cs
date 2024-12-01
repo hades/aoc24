c4fa3f57147dca94ae606dcefe9d9070956d05ba
hades <dev@hades.name>
2024-12-01 08:51:19 +0100

    solve day 1

---
+++ aoc24/Day01.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+
+namespace aoc24;
+
+[ForDay(1)]
+public class Day01 : Solver
+{
+  private ImmutableArray<int> left;
+  private ImmutableArray<int> right;
+
+  public void Presolve(string input)
+  {
+    var pairs = input.Trim().Split("\n").Select(line => Regex.Split(line, @"\s+"));
+    left = pairs.Select(item => int.Parse(item[0])).ToImmutableArray();
+    right = pairs.Select(item => int.Parse(item[1])).ToImmutableArray();
+  }
+
+  public string SolveFirst() => left.Sort().Zip(right.Sort()).Select((pair) => int.Abs(pair.First - pair.Second)).Sum().ToString();
+
+  public string SolveSecond() => left.Select((number) => number * right.Where(v => v == number).Count()).Sum().ToString();
+}
---
+++ tests/TestDay01.cs
@@ -0,0 +1,21 @@
+﻿using aoc24;
+
+namespace tests;
+public class TestDay01
+{
+  [Fact]
+  public void TestFirstPart()
+  {
+    var solver = new Day01();
+    solver.Presolve("3   4\n4   3\n2   5\n1   3\n3   9\n3   3\n");
+    Assert.Equal("11", solver.SolveFirst());
+  }
+
+  [Fact]
+  public void TestSecondPart()
+  {
+    var solver = new Day01();
+    solver.Presolve("3   4\n4   3\n2   5\n1   3\n3   9\n3   3\n");
+    Assert.Equal("31", solver.SolveSecond());
+  }
+}
