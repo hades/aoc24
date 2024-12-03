227e382fbef0a51d05887704cce314ece69ff1ec
hades <dev@hades.name>
2024-12-03 06:39:46 +0100

    solve day 3

---
+++ aoc24/Day03.cs
@@ -0,0 +1,42 @@
+﻿using MathNet.Numerics;
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+
+namespace aoc24;
+
+[ForDay(3)]
+public partial class Day03 : Solver
+{
+  [GeneratedRegex(@"mul[(](\d{1,3}),(\d{1,3})[)]")]
+  private partial Regex mulRegex();
+
+  [GeneratedRegex(@"(do)[(][)]|(don't)[(][)]|(mul)[(](\d{1,3}),(\d{1,3})[)]")]
+  private partial Regex fullRegex();
+
+  private string input;
+
+  public void Presolve(string input)
+  {
+    this.input = input.Trim();
+  }
+
+  public string SolveFirst() => mulRegex().Matches(input)
+      .Select(match => int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value))
+      .Sum().ToString();
+
+  public string SolveSecond()
+  {
+    bool enabled = true;
+    int sum = 0;
+    foreach (Match match in fullRegex().Matches(input)) {
+      if (match.Groups[1].Length > 0) {
+        enabled = true;
+      } else if (match.Groups[2].Length > 0) {
+        enabled = false;
+      } else if (enabled) {
+        sum += int.Parse(match.Groups[4].Value) * int.Parse(match.Groups[5].Value);
+      }
+    }
+    return sum.ToString();
+  }
+}
---
+++ tests/TestDay03.cs
@@ -0,0 +1,21 @@
+﻿using aoc24;
+
+namespace tests;
+public class TestDay03
+{
+  [Fact]
+  public void TestFirstPart()
+  {
+    var solver = new Day03();
+    solver.Presolve("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
+    Assert.Equal("161", solver.SolveFirst());
+  }
+
+  [Fact]
+  public void TestSecondPart()
+  {
+    var solver = new Day03();
+    solver.Presolve("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))");
+    Assert.Equal("48", solver.SolveSecond());
+  }
+}
