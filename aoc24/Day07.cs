f2ea51c6a47ccd838bc27c2b01baac1e818037d4
hades <dev@hades.name>
2024-12-07 13:46:34 +0100

    solve day 7

---
+++ aoc24/Day07.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Immutable;
+
+namespace aoc24;
+
+[ForDay(7)]
+public class Day07 : Solver
+{
+  private ImmutableList<(long, ImmutableList<long>)> equations;
+
+  public void Presolve(string input) {
+    equations = input.Trim().Split("\n")
+      .Select(line => line.Split(": "))
+      .Select(split => (long.Parse(split[0]), split[1].Split(" ").Select(long.Parse).ToImmutableList()))
+      .ToImmutableList();
+  }
+
+  private bool TrySolveWithConcat(long lhs, long head, ImmutableList<long> tail) {
+    var lhs_string = lhs.ToString();
+    var head_string = head.ToString();
+    return lhs_string.Length > head_string.Length &&
+      lhs_string.EndsWith(head_string) &&
+      SolveEquation(long.Parse(lhs_string.Substring(0, lhs_string.Length - head_string.Length)), tail, true);
+  }
+
+  private bool SolveEquation(long lhs, ImmutableList<long> rhs, bool with_concat = false) {
+    if (rhs.Count == 1) return lhs == rhs[0];
+    long head = rhs[rhs.Count - 1];
+    var tail = rhs.GetRange(0, rhs.Count - 1);
+    return (SolveEquation(lhs - head, tail, with_concat))
+      || (lhs % head == 0) && SolveEquation(lhs / head, tail, with_concat)
+      || with_concat && TrySolveWithConcat(lhs, head, tail);
+  }
+
+  public string SolveFirst() => equations
+    .Where(eq => SolveEquation(eq.Item1, eq.Item2))
+    .Select(eq => eq.Item1)
+    .Sum().ToString();
+  public string SolveSecond() => equations
+    .Where(eq => SolveEquation(eq.Item1, eq.Item2, true))
+    .Select(eq => eq.Item1)
+    .Sum().ToString();
+}
---
+++ tests/TestDay07.cs
@@ -0,0 +1,32 @@
+﻿using aoc24;
+
+namespace tests;
+public class TestDay07
+{
+  private static readonly string data = @"190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+".Replace("\r\n", "\n");
+
+  [Fact]
+  public void TestFirstPart()
+  {
+    var solver = new Day07();
+    solver.Presolve(data);
+    Assert.Equal("3749", solver.SolveFirst());
+  }
+
+  [Fact]
+  public void TestSecondPart()
+  {
+    var solver = new Day07();
+    solver.Presolve(data);
+    Assert.Equal("11387", solver.SolveSecond());
+  }
+}
